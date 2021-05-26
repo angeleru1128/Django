0bdcf96d765535c673f805ff0451c9bdc614d599
angeleru1128 <ayakatsutsui1128@gmail.com>
2021-05-27 01:36:36 +0900

    feature/fix/ayaka/views.pyのcreateview変更

--- reviews/urls.py
+++ reviews/urls.py
@@ -6,7 +6,7 @@ app_name = 'reviews'
 urlpatterns = [
     path('', views.ReviewList.as_view(), name='review_list'),
     path('detail/<int:pk>/', views.ReviewDetail.as_view(), name='review_detail'),
-    path('create/', views.review_create, name='review_create'),
+    path('create/', views.ReviewCreate.as_view(), name='review_create'),
     path('update/<int:pk>/', views.review_update, name='review_update'),
     path('delete/<int:pk>/', views.review_delete, name='review_delete'),
 ]
