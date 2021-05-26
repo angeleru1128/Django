5b3d0768990f5fa0437c520fdf16e9fd1c5ae5ad
angeleru1128 <ayakatsutsui1128@gmail.com>
2021-05-27 01:36:15 +0900

    feature/fix/ayaka/urls.pyのcreateview変更

--- reviews/views.py
+++ reviews/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, get_object_or_404, redirect
 from .models import Review
 from .forms import ReviewCreateForm
 from django.views import generic
-
+from django.urls import reverse_lazy
 # Create your views here.
 class ReviewList(generic.ListView):
     model = Review
@@ -12,8 +12,7 @@ def review_list(request):
         'review_list': Review.objects.all().order_by('-created_at'),
     }
     return render(request, 'reviews/review_list.html', context)
-
-
+    
 class ReviewDetail(generic.DetailView):
     model = Review
 
@@ -23,8 +22,11 @@ def review_detail(request, pk):
     }
     return render(request, 'reviews/review_detail.html', context)
 
+class ReviewCreate(generic.CreateView):
+    model = Review
+    form_class = ReviewCreateForm
+    success_url = reverse_lazy('reviews:review_list')
 
-#口コミ作成ページが表示される
 def review_create(request):
     form = ReviewCreateForm(request.POST or None)
     if request.method == 'POST' and form.is_valid():
